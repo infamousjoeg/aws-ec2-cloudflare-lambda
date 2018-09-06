73d93b760b9155575204ac055c0eb29613b23b4f
Joe Garcia <joe@joe-garcia.com>
2018-09-06 09:10:57 -0400

    Added setEnvVarsFromFile to package

--- pkg/func.py
+++ pkg/func.py
@@ -1,5 +1,6 @@
 # Package containing all custom functions
 
+import os
 import boto3
 from collections import defaultdict
 
@@ -23,3 +24,17 @@ def getEC2InstanceIPv4(instance_name):
             public_ip_address = instance.public_ip_address
     
     return public_ip_address
+
+
+# Function to read contents of file
+def setEnvVarsFromFile(filename):
+    with open(filename) as f:
+        for line in f:
+            if 'export' not in line:
+                continue
+            if line.startswith('#'):
+                continue
+            # Remove leading `export `
+            # then, split name / value pair
+            key, value = line.replace('export ', '', 1).strip().split('=', 1)
+            os.environ[key] = value
