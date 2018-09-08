f342e1893c06bf111276f2f4a3a902eab6c756ff
Joe Garcia <joe@joe-garcia.com>
2018-09-07 20:01:20 -0400

    Successfully tested locally - failing in AWS Lambda

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 # main.py
 
 from pkg.func import getEC2InstanceIPv4
-import json
-import CloudFlare
+from pkg.func import setCloudflareARecord
+import os
+
+instance_name = os.getenv('EC2_INSTANCE_NAME')
+cf_zone = os.getenv('CLOUDFLARE_ZONE_ID')
+cf_dns = os.getenv('CLOUDFLARE_DNS_ID')
+cf_email = os.getenv('CLOUDFLARE_EMAIL')
+cf_api_key = os.getenv('CLOUDFLARE_API_KEY')
+cf_arecord_name = os.getenv('CLOUDFLARE_A_NAME')
 
 def handler(event, context):
-    # Get cdemo EC2 Instance Public IPv4 Address
-    cdemoIPv4 = getEC2InstanceIPv4('cdemo')
+    # Get EC2 Instance Public IPv4 Address
+    public_ipv4_address = getEC2InstanceIPv4(instance_name)
+    # FOR TESTING:
+    #public_ipv4_address = "1.2.3.4"
 
-    # Authenticate to Cloudflare API
-    cf = CloudFlare.CloudFlare()
+    # Update Cloudflare A record with the EC2 Instance public IPv4 address
+    cf_status_code = setCloudflareARecord(public_ipv4_address, cf_email, cf_api_key, \
+        cf_arecord_name, cf_zone, cf_dns)
 
-    # Update A Record in Cloudflare DNS
-
-    # SNS Publish Success or Failure
+# FOR TESTING:
+#def main():
+#    handler("nothing", "nothing")
+#
+#if __name__ == "__main__":
+#    main()
