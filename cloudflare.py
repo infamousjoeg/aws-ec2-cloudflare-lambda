024896fdd42fde039affeedb5b531c0577c2b801
Joe Garcia <joe@joe-garcia.com>
2018-09-07 20:00:19 -0400

    Updated for better testing

--- cloudflare.py
+++ cloudflare.py
@@ -7,10 +7,12 @@ cf_email = os.getenv('CLOUDFLARE_EMAIL')
 cf_api_key = os.getenv('CLOUDFLARE_API_KEY')
 cf_zone_id = os.getenv('CLOUDFLARE_ZONE_ID')
 cf_dns_id = os.getenv('CLOUDFLARE_DNS_ID')
+arecord_name = "cdemo.cybr.rocks"
+ip_address = "54.243.159.71"
 
 url = 'https://api.cloudflare.com/client/v4/zones/{0}/dns_records/{1}'.format(cf_zone_id, cf_dns_id)
 
-payload = "{\n\t\"type\":\"A\",\n\t\"name\":\"cdemo.cybr.rocks\",\n\t\"content\":\"54.243.159.71\"\n}"
+payload = "{\n\t\"type\":\"A\",\n\t\"name\":\"" + str(arecord_name) + "\",\n\t\"content\":\"" + str(ip_address) + "\"\n}"
 headers = {
     'X-Auth-Email': cf_email,
     'X-Auth-Key': cf_api_key,
